bde7bcdb9919a22b91c92e5be3562c1661cbe5bf
watermelon <laimin@trustkernel.com>
2018-12-12 13:24:26 +0800

    use CrudRepository interface

--- src/main/java/com/example/example1/controller/UserController.java
+++ src/main/java/com/example/example1/controller/UserController.java
@@ -9,26 +9,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 @Controller
-@RequestMapping(path="/demo")
+@RequestMapping(path = "/demo")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-
-    @GetMapping(path="/add")
-    public void addNewUser(@RequestParam String name,@RequestParam String email)
-    {
-        User u= new User();
+    @GetMapping(path = "/add")
+    public void addNewUser(@RequestParam String name, @RequestParam String email) {
+        User u = new User();
         u.setName(name);
         u.setEmail(email);
         userRepository.save(u);
     }
 
-    @GetMapping(path="/all")
+    @GetMapping(path = "/all")
     @ResponseBody
-    public Iterable<User> getAllUser(){
+    public Iterable<User> getAllUser() {
         return userRepository.findAll();
+    }
+
+    @GetMapping(path = "/info")
+    @ResponseBody
+    public User findOne(@RequestParam Long id) {
+        Optional<User> optional = userRepository.findById(id);
+        return optional.orElse(new User());
+    }
+
+    @GetMapping(path = "/delete")
+    public void delete(@RequestParam Long id) {
+        userRepository.deleteById(id);
     }
 }
